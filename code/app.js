cb23dab5011a294b25bd23bf8d002d2b8dd63e9e
Peywan Sabir <peywan1999@icloud.com>
2023-11-26 01:03:30 +0100

    done exercises with arrays and objects.

--- code/app.js
+++ code/app.js
@@ -1,30 +1,29 @@
-  // You can iterate over items in an array using forEach. To use forEach,
-  // you define a function which will be invoked once for every element in
-  // the array. When the function is invoked, it will receive the item from
-  // the array as an argument.
-  // Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+// This task pulls together the previous two tasks to practice a more "real" data
+// structure you'd see regularly as a web developer.
+// Consider the following array of product objects:
+const products = [
+  { name: "Camera", brand: "Canon", model: "EOS 70D" },
+  { name: "Camera", brand: "Nikon", model: "D3400" },
+  { name: "Camera", brand: "GoPro", model: "Hero 4" },
+  { name: "Drone", brand: "DJI", model: "Phantom" },
+  { name: "Drone", brand: "GoPro", model: "Karma" },
+];
 
+// Task 1: Write a .forEach function which iterates through the products
+// and uses console.log() to print the name and brand of each item.
+const log = console.log;
 
+// products.forEach(function (product) {
+//   log(product.name, product.brand, product.model);
+// });
 
-  
-  // This array of dog breeds will be used for the examples below. Don't modify it here!
-  const dogs = ["Labrador", "Beagle", "Poodle", "Pug", "Chihuahua"]
+// Task 2: Use .map() to iterate over the products and return the model from
+// each product. Assign the result to a variable called `models` and use console.log()
+// to print it.
 
-  // When invoked, this function will print "The {dogName} barks." You'll use this below. Don't modify it here!
-  const printDogName = (dogName) => {
-    console.log("The " + dogName + " barks.")
-  }
+let models = products.map((product) => {
+  let models = product.model;
+  return models;
+});
 
-  // Task 1: Pass the printDogName function into a .forEach call on the `dogs` array, to
-  // make it print "The {dogName} barks." for each item in the array.
-
-
-  // Task 2: Create a new function called `printDogNameUppercase` which works similarly
-  // to the `printDogName` function but makes the string uppercase. (See https://www.w3schools.com/jsref/jsref_touppercase.asp
-  // for how to make a string uppercase). Use .forEach to run the function for each
-  // dog in the dogs array.
-
-
-  // Task 3: Write a forEach function which uses an anonymous function instead of defining a
-  // named function like the examples above (hint; the lecture material used anonymous functions)
-  // and uses console.log to print "The {dogName} needs to go for a walk!".
+log(models);
